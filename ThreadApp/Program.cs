1ee929829a11f1cdabfd825cb5d0ef13f18c44dd
alex-toy <alexei.80@hotmail.fr>
2023-12-29 10:00:19 +0100

    init

---
+++ ThreadApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ThreadApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread mainThread = Thread.CurrentThread;
+            mainThread.Name = "Main thread";
+            Console.WriteLine($"Thread Name = {mainThread.Name}");
+            Console.WriteLine($"My current Thread = {Thread.CurrentThread.Name}");
+        }
+    }
+}
